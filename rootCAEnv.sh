f5b816f85a2aeefd105ea8768a2f702daecc16ed
Ken <143446633+Foren-Ken@users.noreply.github.com>
2024-09-17 18:39:20 -0400

    Update rootCAEnv.sh

--- rootCAEnv.sh
+++ rootCAEnv.sh
@@ -8,3 +8,17 @@ chmod 700 private
 touch index.txt
 echo 1000 > serial
 curl https://jamielinux.com/docs/openssl-certificate-authority/_downloads/root-config.txt > openssl.cnf
+
+# altering the openssl.cnf file
+nano openssl.cnf
+
+# creating the root key
+openssl genrsa -aes256 -out private/ca.key.pem 4096
+chmod 400 private/ca.key.pem
+
+# Creating root cert
+openssl req -config openssl.cnf -key private/ca.key.pem -new -x509 -days 7300 -sha256 -extensions v3_ca -out certs/ca.cert.pem
+chmod 444 certs/ca.cert.pem
+
+# verification of root cert. 
+openssl x509 -noout -text -in certs/ca.cert.pem | more
